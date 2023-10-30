aa0b50396ec2f213c971bad6f6b8d3b03fd2cbdd
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-30 14:08:01 +0300

    Add files via upload

--- User.js
+++ User.js
@@ -1,12 +1,22 @@
-import Wrapper from "../helpers/Wrapper";
+import React from "react";
 
 function User(){
 
+    // Components Wrapper is a root element
+    // return(
+    //     <Wrapper>
+    //         <h2>Some Title</h2>
+    //         <p>Some text</p>
+    //     </Wrapper>
+    // )
+
+    // Create root element using React.Fragment
     return(
-        <Wrapper>
-            <h2>Some Title</h2>
-            <p>Some text</p>
-        </Wrapper>
+        <React.Fragment>
+            <h2>Some User Title</h2>
+            <p>Some User text</p>
+        </React.Fragment>
     )
+
 }
 export default User;
